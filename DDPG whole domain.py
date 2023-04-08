a2001ff91adfcd6f21e69f1cf63e56208fee80df
BOYJZ <111651791+BOYJZ@users.noreply.github.com>
2023-04-08 14:42:36 -0500

    Add files via upload

--- DDPG whole domain.py
+++ DDPG whole domain.py
@@ -12,16 +12,14 @@ class Actor(nn.Module):
         self.layer_1 = nn.Linear(state_dim, 512)
         self.layer_2 = nn.Linear(512, 512)
         self.layer_3 = nn.Linear(512, 256)
-        self.layer_4 = nn.Linear(256, 128)  # Added hidden layer
-        self.layer_5 = nn.Linear(128, action_dim)  # Modified to use the output of the added layer
+        self.layer_4 = nn.Linear(256, action_dim)
         self.max_action = max_action
 
     def forward(self, x):
         x = nn.ReLU()(self.layer_1(x))
         x = nn.ReLU()(self.layer_2(x))
         x = nn.ReLU()(self.layer_3(x))
-        x = nn.ReLU()(self.layer_4(x))  # Added ReLU activation for the new layer
-        x = self.max_action * nn.Tanh()(self.layer_5(x))
+        x = self.max_action * nn.Tanh()(self.layer_4(x))
         return x
 
 # Define the Critic network
@@ -31,20 +29,14 @@ class Critic(nn.Module):
         self.layer_1 = nn.Linear(state_dim + action_dim, 512)
         self.layer_2 = nn.Linear(512, 512)
         self.layer_3 = nn.Linear(512, 256)
-        self.layer_4 = nn.Linear(256, 128)  # Added hidden layer
-        self.layer_5 = nn.Linear(128, 1)  # Modified to use the output of the added layer
+        self.layer_4 = nn.Linear(256, 1)
 
     def forward(self, x, u):
         x = nn.ReLU()(self.layer_1(torch.cat([x, u], 1)))
         x = nn.ReLU()(self.layer_2(x))
         x = nn.ReLU()(self.layer_3(x))
-        x = nn.ReLU()(self.layer_4(x))  # Added ReLU activation for the new layer
-        x = self.layer_5(x)
+        x = self.layer_4(x)
         return x
-
-
-
-
 
 class DDPG():
     def __init__(self, state_dim, action_dim, max_action):
@@ -106,31 +98,29 @@ class DDPG():
         state = torch.FloatTensor(state.reshape(1, -1))
         return self.actor(state).cpu().data.numpy().flatten()
 
+
 def noisy_gaussian(state):
     x, y, z = state
-    SNR = 100000000
+    SNR = 0.1
     mu_x, mu_y, mu_z = 0, 0, 0
     sigma_x, sigma_y, sigma_z = 1, 1, 1
     f = np.exp(-((x - mu_x) ** 2 / (2 * sigma_x ** 2) + (y - mu_y) ** 2 / (2 * sigma_y ** 2) + (z - mu_z) ** 2 / (2 * sigma_z ** 2)))
     noisy_f = f + np.random.normal(0, 1 / SNR, f.shape)
     return noisy_f
 
-# DDPG settings
+#DDPG settings
 state_dim = 3
-action_dim = 3
-max_action = 20
+action_dim = 4 # 3 dimensions for the searching center and 1 dimension for the radius
+max_action = 2 # The maximum value of each component of the action
 
-# Initialize DDPG agent
+#Initialize DDPG agent
 agent = DDPG(state_dim, action_dim, max_action)
 
-n_episodes = 10
+n_episodes = 2
 max_steps = 50
 
-# Exploration noise
+#Exploration noise
 exploration_noise = 0.01
-
-
-
 
 max_reward = float('-inf')
 max_state = None
@@ -142,10 +132,16 @@ for episode in range(n_episodes):
     for step in range(max_steps):
         action = agent.select_action(state)
         action = (action + exploration_noise * np.random.randn(action_dim)).clip(-max_action, max_action)
-        action_scaling = 0.005  # Adjust this value to control the step size
-        next_state = np.clip(state + action_scaling * action, -2, 2)  # Apply action_scaling to the action
+        
+        # Sample a point within the sphere defined by the center and radius given by the action
+        center, radius = action[:3], action[3]
+        random_direction = np.random.randn(3)
+        random_direction /= np.linalg.norm(random_direction)
+        random_distance = radius * np.random.random() ** (1/3)
+        next_state = center + random_distance * random_direction
+
         reward_factor = 100
-        reward = reward_factor * noisy_gaussian(state)
+        reward = reward_factor * noisy_gaussian(next_state)
         done = False
 
         agent.add_to_memory(state, action, next_state, reward, done)
@@ -153,13 +149,11 @@ for episode in range(n_episodes):
 
         state = next_state
         episode_reward += reward
-        print('state,reward',state,reward/reward_factor)
+
         if reward > max_reward:
             max_reward = reward
-            max_state = state
+            max_state = next_state
             distance = np.sqrt(max_state[0]**2 + max_state[1]**2 + max_state[2]**2)
-            print("Maximum location:", max_state,reward/reward_factor)
-            #print(distance <0.224)
 
         if step == max_steps - 1:
             print("Episode", episode, "Reward", episode_reward/reward_factor)
@@ -169,10 +163,3 @@ print("Maximum location:", max_state, max_reward/reward_factor)
 distance = np.sqrt(max_state[0]**2 + max_state[1]**2 + max_state[2]**2)
 success = distance <0.224
 print(success,distance)
-
-
-
-
-
-        
-
--- DDPG-optimized assessment.py
+++ DDPG-optimized assessment.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 import random
 from collections import deque
 
@@ -12,16 +13,14 @@ class Actor(nn.Module):
         self.layer_1 = nn.Linear(state_dim, 512)
         self.layer_2 = nn.Linear(512, 512)
         self.layer_3 = nn.Linear(512, 256)
-        self.layer_4 = nn.Linear(256, 128)  # Added hidden layer
-        self.layer_5 = nn.Linear(128, action_dim)  # Modified to use the output of the added layer
+        self.layer_4 = nn.Linear(256, action_dim)
         self.max_action = max_action
 
     def forward(self, x):
         x = nn.ReLU()(self.layer_1(x))
         x = nn.ReLU()(self.layer_2(x))
         x = nn.ReLU()(self.layer_3(x))
-        x = nn.ReLU()(self.layer_4(x))  # Added ReLU activation for the new layer
-        x = self.max_action * nn.Tanh()(self.layer_5(x))
+        x = self.max_action * nn.Tanh()(self.layer_4(x))
         return x
 
 # Define the Critic network
@@ -31,20 +30,14 @@ class Critic(nn.Module):
         self.layer_1 = nn.Linear(state_dim + action_dim, 512)
         self.layer_2 = nn.Linear(512, 512)
         self.layer_3 = nn.Linear(512, 256)
-        self.layer_4 = nn.Linear(256, 128)  # Added hidden layer
-        self.layer_5 = nn.Linear(128, 1)  # Modified to use the output of the added layer
+        self.layer_4 = nn.Linear(256, 1)
 
     def forward(self, x, u):
         x = nn.ReLU()(self.layer_1(torch.cat([x, u], 1)))
         x = nn.ReLU()(self.layer_2(x))
         x = nn.ReLU()(self.layer_3(x))
-        x = nn.ReLU()(self.layer_4(x))  # Added ReLU activation for the new layer
-        x = self.layer_5(x)
+        x = self.layer_4(x)
         return x
-
-
-
-
 
 class DDPG():
     def __init__(self, state_dim, action_dim, max_action):
@@ -106,73 +99,120 @@ class DDPG():
         state = torch.FloatTensor(state.reshape(1, -1))
         return self.actor(state).cpu().data.numpy().flatten()
 
-def noisy_gaussian(state):
+
+def noisy_gaussian(state, snr):
     x, y, z = state
-    SNR = 100000000
     mu_x, mu_y, mu_z = 0, 0, 0
     sigma_x, sigma_y, sigma_z = 1, 1, 1
     f = np.exp(-((x - mu_x) ** 2 / (2 * sigma_x ** 2) + (y - mu_y) ** 2 / (2 * sigma_y ** 2) + (z - mu_z) ** 2 / (2 * sigma_z ** 2)))
-    noisy_f = f + np.random.normal(0, 1 / SNR, f.shape)
+    noisy_f = f + np.random.normal(0, 1 / snr, f.shape)
     return noisy_f
 
-# DDPG settings
-state_dim = 3
-action_dim = 3
-max_action = 20
+def train(snr):
+    #DDPG settings
+    state_dim = 3
+    action_dim = 4 # 3 dimensions for the searching center and 1 dimension for the radius
+    max_action = 2 # The maximum value of each component of the action
 
-# Initialize DDPG agent
-agent = DDPG(state_dim, action_dim, max_action)
+    #Initialize DDPG agent
+    agent = DDPG(state_dim, action_dim, max_action)
+    
+    n_episodes = 2
+    max_steps = 50
+    
+    #Exploration noise
+    exploration_noise = 0.01
 
-n_episodes = 10
-max_steps = 50
+    max_reward = float('-inf')
+    max_state = None
 
-# Exploration noise
-exploration_noise = 0.01
+    for episode in range(n_episodes):
+        state = np.random.uniform(-1, 1, state_dim)
+        episode_reward = 0
 
-
-
-
-max_reward = float('-inf')
-max_state = None
-
-for episode in range(n_episodes):
-    state = np.random.uniform(-1, 1, state_dim)
-    episode_reward = 0
-
-    for step in range(max_steps):
-        action = agent.select_action(state)
-        action = (action + exploration_noise * np.random.randn(action_dim)).clip(-max_action, max_action)
-        action_scaling = 0.005  # Adjust this value to control the step size
-        next_state = np.clip(state + action_scaling * action, -2, 2)  # Apply action_scaling to the action
-        reward_factor = 100
-        reward = reward_factor * noisy_gaussian(state)
-        done = False
-
-        agent.add_to_memory(state, action, next_state, reward, done)
-        agent.train()
-
-        state = next_state
-        episode_reward += reward
-        print('state,reward',state,reward/reward_factor)
-        if reward > max_reward:
-            max_reward = reward
-            max_state = state
-            distance = np.sqrt(max_state[0]**2 + max_state[1]**2 + max_state[2]**2)
-            print("Maximum location:", max_state,reward/reward_factor)
-            #print(distance <0.224)
-
-        if step == max_steps - 1:
-            print("Episode", episode, "Reward", episode_reward/reward_factor)
-            print(state,np.sqrt(state[0]**2 + state[1]**2 + state[2]**2)<0.225)
-            break
-print("Maximum location:", max_state, max_reward/reward_factor)
-distance = np.sqrt(max_state[0]**2 + max_state[1]**2 + max_state[2]**2)
-success = distance <0.224
-print(success,distance)
-
-
-
-
-
+        for step in range(max_steps):
+            action = agent.select_action(state)
+            action = (action + exploration_noise * np.random.randn(action_dim)).clip(-max_action, max_action)
         
+            # Sample a point within the sphere defined by the center and radius given by the action
+            center, radius = action[:3], action[3]
+            random_direction = np.random.randn(3)
+            random_direction /= np.linalg.norm(random_direction)
+            random_distance = radius * np.random.random() ** (1/3)
+            next_state = center + random_distance * random_direction
+            
+            reward_factor = 100
+            reward = reward_factor * noisy_gaussian(next_state, snr)
+            done = False
+            
+            agent.add_to_memory(state, action, next_state, reward, done)
+            agent.train()
+            
+            state = next_state
+            episode_reward += reward
+            #print('state,reward',state,reward/reward_factor)
+            if reward > max_reward:
+                max_reward = reward
+                max_state = next_state
+                distance = np.sqrt(max_state[0]**2 + max_state[1]**2 + max_state[2]**2)
+
+            if step == max_steps - 1:
+                #print("Episode", episode, "Reward", episode_reward/reward_factor)
+                #print(state,np.sqrt(state[0]**2 + state[1]**2 + state[2]**2)<0.225)
+                break
+        #print("Maximum location:", max_state, max_reward/reward_factor)
+        distance = np.sqrt(max_state[0]**2 + max_state[1]**2 + max_state[2]**2)
+        success = distance <0.225
+        #print(success,distance)
+    return success
+
+def draw_pro_snr():
+    snr_values = np.arange(0.01,10,1)
+    num_experiments = 100
+
+    success_probabilities = []
+
+    for snr in snr_values:
+        print(snr)
+        success_count = 0
+        for _ in range(num_experiments):
+            success = train(snr)
+            if success:
+                success_count += 1
+
+        success_probability = success_count / num_experiments
+        success_probabilities.append(success_probability)
+
+    plt.plot(snr_values, success_probabilities)
+    plt.xlabel('SNR')
+    plt.ylabel('Probability of Success')
+    plt.title('Probability of Success vs. SNR')
+    plt.show()
+    
+draw_pro_snr()
+#train(100)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
